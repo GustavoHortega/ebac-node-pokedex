d7f2e5173368df6aaff87b79eeb29456a818ebab
Gustavo Hortega <gustavohortega80@gmail.com>
2024-12-03 15:11:45 -0400

    Faça a integração com a pokeapi para a página de batalha.

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const path = require('path');
 
 const { connect } = require('./models');
 
+const batalhaRouter = require('./routes/batalha');
 const pokemonsRouter = require('./routes/pokemons');
 const expressEjsLayouts = require('express-ejs-layouts');
 
 const app = express();
 
+//EJS e Layouts
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressEjsLayouts);
-app.use(express.static('public'));
+
+//Arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 //rotas declaradas
 app.use('/pokemons', pokemonsRouter);
+app.use('/batalha', batalhaRouter);
 
 const port = 3000;
 
