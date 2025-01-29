22d0f165ef1939a872f72f9ff41c07cca3ca70a9
Gustavo Hortega <gustavohortega80@gmail.com>
2025-01-29 09:43:25 -0400

    Crie o schma para o usuário.

---
+++ models/usuario.js
@@ -0,0 +1,26 @@
+const { Schema } = require('mongoose');
+
+const Usuario = new Schema({
+    nome:{
+        type: String,
+        required: true,
+        min: 4
+    },
+    email:{
+        type: String,
+        required: true,
+        min: 4,
+        unique: true, //Prop. de unicidade.
+        validator: function(v) {
+            return v.match('@');
+        },
+        message: props => `${props.value} não é um e-mail válido`
+    },
+    senha:{
+        type: String,
+        required: true
+    }
+
+});
+
+module.exports = Usuario;
